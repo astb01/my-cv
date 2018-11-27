cd2ae174361e4f4a4b6fb81684429e3ea886620e
Assad Ahmad <assad.ahmad1@dwp.gsi.gov.uk>
2018-11-27 12:32:48 +0000

    Added ignore config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,19 @@ module.exports = {
   rules: {
     semi: ["error", "always"],
     quotes: ["error", "double"]
-  }
+  },
+  env: {
+    commonjs: true,
+    node: true,
+    mocha: true
+  },
+  overrides: [
+    {
+      files: "*-spec.js",
+      rules: {
+        "no-unused-expressions": "off",
+        quotes: "off"
+      }
+    }
+  ]
 };
